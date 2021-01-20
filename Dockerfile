9db830df1ea31476eaf3f0ea21b08f06fec8b9a8
JJ Asghar <jjasghar@gmail.com>
2021-01-20 12:25:49 -0600

    inital commit
    Signed-off-by: JJ Asghar <jjasghar@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM fedora:latest
+
+RUN dnf upgrade -y
+RUN dnf install -y \
+    wget \
+    unzip 
+
+RUN curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" && \
+chmod +x ./kubectl && \
+mv ./kubectl /usr/local/bin/kubectl && \
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash 
+
+
+RUN curl -fsSL https://clis.cloud.ibm.com/install/linux | sh && \
+ibmcloud config --check-version=false
+
+WORKDIR /
+RUN mkdir /scripts
+COPY scripts/run.sh /scripts/run.sh
+
+ENTRYPOINT ["/scripts/run.sh"]
---
+++ scripts/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ibmcloud plugin install -f kubernetes-service
+sleep 20
+ibmcloud login --apikey $1 -r "us-south"
+sleep 20
+ibmcloud ks cluster config --cluster $2
+sleep 10
+helm repo add kube-agent https://moficodes.github.io/kube-agent-helm/
+sleep 10
+SUBDOMAIN=$(ibmcloud ks cluster get --cluster $2 | grep Subdomain | awk '{ print $NF }')
+SECRET=$(ibmcloud ks cluster get --cluster $2 | grep Secret | awk '{ print $NF }')
+sleep 10
+kubectl create ns agent
+helm install agent kube-agent/kube-agent --set namespace=agent --set ingress.host=agent.$SUBDOMAIN --set ingress.secret=$SECRET --namespace=agent --set secret.jwtsecret=anystringbecausemofitoldmeto
