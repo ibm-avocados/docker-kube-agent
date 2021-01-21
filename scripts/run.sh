045b7cb0d939168fcd7f1e58a056a1f040dc889f
JJ Asghar <jjasghar@gmail.com>
2021-01-21 08:47:02 -0600

    updated the run script to actually work :facepalm:
    Signed-off-by: JJ Asghar <jjasghar@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,10 +6,12 @@ ibmcloud login --apikey $1 -r "us-south"
 sleep 20
 ibmcloud ks cluster config --cluster $2
 sleep 10
+dnf install -y openssl
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add kube-agent https://moficodes.github.io/kube-agent-helm/
 sleep 10
-SUBDOMAIN=$(ibmcloud ks cluster get --cluster $2 | grep Subdomain | awk '{ print $NF }')
-SECRET=$(ibmcloud ks cluster get --cluster $2 | grep Secret | awk '{ print $NF }')
+SUBDOMAIN=`ibmcloud ks cluster get --cluster "$2" | grep Subdomain | awk '{ print $NF }'`
+SECRET=`ibmcloud ks cluster get --cluster "$2" | grep Secret | awk '{ print $NF }'`
 sleep 10
 kubectl create ns agent
 helm install agent kube-agent/kube-agent --set namespace=agent --set ingress.host=agent.$SUBDOMAIN --set ingress.secret=$SECRET --namespace=agent --set secret.jwtsecret=anystringbecausemofitoldmeto
